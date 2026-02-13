228a207880e007796d3d1256307d5b27e28f1f23
Robotics Laptop 1 <robotics1@student.rockway.ca>
2026-02-13 18:25:32 -0500

    added talon lib

--- src/main/java/frc/robot/subsystems/climb/ClimbReal.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbReal.java
@@ -1,6 +1,11 @@
 package frc.robot.subsystems.climb;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import frc.robot.Constants.CAN;
+
 public class ClimbReal implements ClimbIO {
+  private final TalonFX motor = new TalonFX(CAN.CLIMB);
+
   public ClimbReal() {}
 
   @Override
