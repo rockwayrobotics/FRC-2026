f08e8d616a57def37465b6ce96c31e6529b7e402
Your Name <you@example.com>
2026-02-17 17:05:06 -0500

    Update intake methods

--- src/main/java/frc/robot/subsystems/intake/IntakeReal.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeReal.java
@@ -3,7 +3,9 @@ package frc.robot.subsystems.intake;
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
+import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
@@ -13,11 +15,10 @@ import frc.robot.util.SparkUtil;
 import java.util.function.DoubleSupplier;
 
 public class IntakeReal implements IntakeIO {
-  private final SparkBase retract = new SparkMax(CAN.INTAKE_RETRACTION, MotorType.kBrushless);
-  private final SparkBase extendingRoller =
-      new SparkMax(CAN.INTAKE_EXTENDING_ROLLERS, MotorType.kBrushless);
-  private final RelativeEncoder retractEncoder = retract.getEncoder();
-  private final RelativeEncoder extendingEncoder = extendingRoller.getEncoder();
+  private final SparkBase extendRetractMotor = new SparkMax(CAN.INTAKE_RETRACTION, MotorType.kBrushless);
+  private final SparkBase roller = new SparkMax(CAN.INTAKE_EXTENDING_ROLLERS, MotorType.kBrushless);
+  private final RelativeEncoder retractEncoder = extendRetractMotor.getEncoder();
+  private final RelativeEncoder extendingEncoder = roller.getEncoder();
 
   public IntakeReal() {
     var retractConfig = new SparkMaxConfig();
@@ -30,37 +31,48 @@ public class IntakeReal implements IntakeIO {
         .forwardSoftLimitEnabled(true)
         .reverseSoftLimitEnabled(true);
     SparkUtil.tryUntilOk(
-        retract,
+        extendRetractMotor,
         5,
         () ->
-            retract.configure(
+            extendRetractMotor.configure(
                 retractConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
 
     var extendingConfig = new SparkMaxConfig();
     extendingConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(60).voltageCompensation(12.0);
     SparkUtil.tryUntilOk(
-        extendingRoller,
+        roller,
         5,
         () ->
-            extendingRoller.configure(
+            roller.configure(
                 extendingConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters));
   }
 
   @Override
   public void updateInputs(IntakeIOInputs inputs) {
-    SparkUtil.ifOk(retract, retractEncoder::getPosition, (value) -> inputs.retractPosition = value);
+    SparkUtil.ifOk(extendRetractMotor, retractEncoder::getPosition, (value) -> inputs.retractPosition = value);
     SparkUtil.ifOk(
-        retract,
-        new DoubleSupplier[] {retract::getAppliedOutput, retract::getBusVoltage},
+        extendRetractMotor,
+        new DoubleSupplier[] {extendRetractMotor::getAppliedOutput, extendRetractMotor::getBusVoltage},
         (values) -> inputs.retractAppliedVolts = values[0] * values[1]);
 
     SparkUtil.ifOk(
-        extendingRoller,
+        roller,
         extendingEncoder::getVelocity,
         (value) -> inputs.extendingVelocityRadsPerSec = value);
     SparkUtil.ifOk(
-        extendingRoller,
-        new DoubleSupplier[] {extendingRoller::getAppliedOutput, extendingRoller::getBusVoltage},
+        roller,
+        new DoubleSupplier[] {roller::getAppliedOutput, roller::getBusVoltage},
         (values) -> inputs.extendingAppliedVolts = values[0] * values[1]);
+  }
+
+  public void runIntakeRoller(double volts) {
+    roller.setVoltage(volts);
+  }
+
+  public void intakeInOut(double limit) {
+    extendRetractMotor
+        .getClosedLoopController()
+        .setSetpoint(
+            limit, ControlType.kPosition, ClosedLoopSlot.kSlot0);
   }
 }
